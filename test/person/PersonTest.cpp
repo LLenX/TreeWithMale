548475fc7f39679cc6b3a4d0c9b4fd219f4c85f6
Alvin Xue <19786654@qq.com>
2016-11-18 08:19:55 +0800

    test wife done

--- test/person/PersonTest.cpp
+++ test/person/PersonTest.cpp
@@ -78,3 +78,40 @@ TEST_F(MaleTest, TestSibling) {
     EXPECT_EQ(*johns_siblings->front(), *catherine);
     EXPECT_EQ(*catherines_sibilings->front(), *john);
 }
+
+class WifeTest : public testing::Test {
+  protected:
+    virtual void SetUp() {
+        chen_xu = std::dynamic_pointer_cast<Male>(
+            Person::CreateAncestor(Person::Info("15331111", "ChenXu")));
+        chen_yuxiang = chen_xu->Marry(Person::Info("15332222", "ChenZiXiang"));
+    }
+
+    std::shared_ptr<Male> chen_xu;
+    std::shared_ptr<Wife> chen_yuxiang;
+};
+
+TEST_F(WifeTest, TestDivorce) {
+    ASSERT_TRUE(chen_yuxiang->IsMarried());
+    ASSERT_TRUE(chen_xu->IsMarried());
+
+    chen_yuxiang->Divorce();
+
+    EXPECT_FALSE(chen_yuxiang->IsMarried());
+    EXPECT_FALSE(chen_xu->IsMarried());
+    EXPECT_EQ(*chen_xu->ExWives()->front(), *chen_yuxiang);
+}
+
+TEST_F(WifeTest, TestBirth) {
+    auto john = chen_yuxiang->GiveBirthTo(
+        Person::Info("Child Id", "John"),
+        Person::MALE), catherine = chen_yuxiang->GiveBirthTo(
+        Person::Info("Child Id2", "Catherine"), Person::FEMALE);
+    std::shared_ptr<Person::Vector<
+        BloodRelation>> johns_siblings = john->Siblings(), catherines_sibilings = catherine->Siblings();
+
+    EXPECT_EQ(johns_siblings->size(), 1);
+    EXPECT_EQ(catherines_sibilings->size(), 1);
+    EXPECT_EQ(*johns_siblings->front(), *catherine);
+    EXPECT_EQ(*catherines_sibilings->front(), *john);
+}
